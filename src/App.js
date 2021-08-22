0cbdd201569a4dcc964084386bb5658c7e0f35df
Hemerton Nobre <nobre_saxx@yahoo.com.br>
2021-08-21 23:10:10 -0300

    instalando e configurando redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
+import React from 'react';
+import { connect } from 'react-redux';
 
-
-
-function App() {
+const Page =(props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-                      <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>Opa sera que vai funcionar</div>
   );
 }
+const mapStateToProps = (state) => {
+  return{
+    user:state.user
+  };
+}
 
-export default App;
+const mapDisphatchToProps = (dispatch) => {
+  return {
+
+  };
+
+}
+
+export default connect(mapStateToProps, mapDisphatchToProps) (Page);
