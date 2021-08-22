dc200c12e4a3997b1f3a1f1962e73245ceeed9f9
Hemerton Nobre <nobre_saxx@yahoo.com.br>
2021-08-22 10:58:37 -0300

    criando as rotas  iniciais da página

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './Routes';
 
 const Page =(props) => {
   return (
-    <div>Opa sera que vai funcionar</div>
+    <BrowserRouter>
+       <Routes />
+
+      
+    </BrowserRouter>   
   );
 }
 const mapStateToProps = (state) => {
