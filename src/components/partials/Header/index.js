b3af97aa069a3b2fd8f561be1e03142c1ef25241
Hemerton Nobre <nobre_saxx@yahoo.com.br>
2021-08-24 22:15:33 -0300

    criando o menu, instalando cookies

--- src/components/partials/Header/index.js
+++ src/components/partials/Header/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import { HeaderArea } from './styled';
 
+import { isLogged } from '../../../helpers/AuthHandler';
+
 const Header = () => {
+    let logged =isLogged();
+
     return (
         <HeaderArea>
             <div className="container">
@@ -13,6 +17,27 @@ const Header = () => {
                         <span className="logo-3">X</span>
                     </Link>
                 </div>
+                <nav>
+                    <ul>
+                        {logged &&
+                            <>
+                                <li><Link to="/my-account">Minha Conta</Link></li>
+                                <li><Link to="/logout">Sair</Link></li>
+                                <li><Link to="post-an-ad" className="button">Poste um anúnicio</Link></li>
+                            </>
+
+                        }
+                        {!logged &&
+                            <>
+                                <li><Link to="/signin">Login</Link></li>
+                                <li><Link to="sigup">Cadastrar</Link></li>
+                                <li><Link to="signin" className="button">Poste um anúnicio</Link></li>
+
+                            </>
+
+                        }
+                    </ul>
+                </nav>
             </div>
         </HeaderArea>
     );
